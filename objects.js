71b117dcd9e65de2ad37222dd2b84f40ea856c6e
Trevor Seitz <trevinla@gmail.com>
2017-10-04 15:46:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,7 @@ var playlist = new Object({artistName: 'songTitle'});
 
 function updatePlaylist(playlist, artistName, songTitle) {
 var playlist = ({artistName: 'songTitle'});
+return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
